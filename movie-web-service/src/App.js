559b9b8e798ad299619286dec88f0c21afdbf365
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-13 12:11:44 +0900

    #7.3 Movie app part1 - json

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -3,26 +3,29 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(()=>{
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, [])
-  // console > network 에서 tickers를 request했다는 것을 확인할 수 있다.
-  // data를 받아옴
-  // coins를 얻었다면 loading을 false로 변경
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  // async, await를 사용하기 위해 getMovies라는 함수 생성
+
+  useEffect(()=>getMovies(),[]);
   return (
     <div>
-      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? <strong>Loading...</strong> 
-      : <select>
-        {coins.map((coin)=><option>{coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD</option>)}
-      </select>}
-      
+      {loading ? <h1>Loading...</h1> : 
+        <div>{movies.map((item)=>
+          <div key={item.key}>
+            <img src={item.medium_cover_image} />
+            <h2>{item.title}</h2>
+            <p>{item.summary}</p>
+            <ul>
+              {item.genres.map(item => <li key={item}>{item}</li>)}
+            </ul>
+          </div>)}
+        </div>
+      }
     </div>
   );
 }
