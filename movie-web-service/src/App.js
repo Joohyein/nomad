7a8b37001cda476e585a9b72c78f2f1e6e399fbe
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-13 09:46:48 +0900

    Todo List part1

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();  
+    if(todo === "") return;
+    setTodo("");
+    setTodos((currentArray)=>[todo, ...currentArray]);
+    // 기존 배열에 추가한 값이 배열의 가장 앞에 추가된다.
+  }
+  console.log(todos);
+  // state를 직접적으로 수정하지 않는다.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Todo ({todos.length})</h1>
+      <form>
+        <input 
+          onChange={onChange}
+          value={todo}
+          type="text"
+          placeholder='Write your to do...'
+        />
+        <button onClick={onSubmit}>Add to do</button>
+      </form>     
     </div>
   );
 }
