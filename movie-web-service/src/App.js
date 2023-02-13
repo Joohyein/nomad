a1b37c56f2ac3a3c586a14130ffd5c2dcd3d3fc1
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-13 16:08:42 +0900

    component divide

--- movie-web-service/src/App.js
+++ movie-web-service/src/App.js
@@ -1,33 +1,9 @@
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async() => {
-    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  }
-  // async, await를 사용하기 위해 getMovies라는 함수 생성
-
-  useEffect(()=>getMovies(),[]);
-  return (
-    <div>
-      {loading ? <h1>Loading...</h1> : 
-        <div>{movies.map((item)=>
-          <div key={item.key}>
-            <img src={item.medium_cover_image} />
-            <h2>{item.title}</h2>
-            <p>{item.summary}</p>
-            <ul>
-              {item.genres.map(item => <li key={item}>{item}</li>)}
-            </ul>
-          </div>)}
-        </div>
-      }
-    </div>
-  );
+  
 }
 
 export default App;
+
+// routersms url을 보고있는 component이다.
---
+++ movie-web-service/src/routes/Home.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import Movie from './components/Movie';
+
+function Home() {
+    const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async() => {
+    const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  }
+  // async, await를 사용하기 위해 getMovies라는 함수 생성
+
+  useEffect(()=>getMovies(),[]);
+  return (
+    <div>
+      {loading ? <h1>Loading...</h1> : 
+        (
+          <div>
+            {movies.map((movie)=><Movie coverImg={movie.medium_cover_image} title={movie.title} genres={movie.genres} />)}
+          </div>
+        )
+      }
+    </div>
+  );
+}
+
+export default Home;
