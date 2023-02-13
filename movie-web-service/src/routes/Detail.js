95de30e5bffcf05053ec90cf2c9b9a7a8b3c60f1
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-13 19:58:06 +0900

    fix: 각 컴포넌트별 키 추가

--- movie-web-service/src/routes/Detail.js
+++ movie-web-service/src/routes/Detail.js
@@ -1,9 +1,59 @@
-import React from 'react'
+// import React, { useEffect, useState } from 'react'
+// import { useParams } from 'react-router-dom'
 
-function Detail() {
-  return (
-    <div>Detail</div>
-  )
+// function Detail() {
+//     const [loading, setLoading] = useState(true);
+//     const [movies, setMovies] = useState([]);
+
+//     const {id} = useParams();
+//     console.log(id);
+    
+//     useEffect(()=>{
+//         getMovie();
+//     },[]);
+ 
+//     const getMovie = async() => {
+//         const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
+//         setMovies(json.data.movie);
+//         setLoading(false);
+//     }
+
+    
+//     console.log(movies);
+//   return (
+//     <div>
+//         <h1>hi</h1>
+//         {loading ? (<h1>Loading</h1>): (
+//             <div>
+//                 <div>{movies.title}</div>
+//             </div>
+//         )}
+//     </div>
+
+//   )
+// }
+// export default Detail;
+
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+function Datail() {
+    const [movie, setMovie] = useState([]);
+    const { id } = useParams();
+    const getMovie = async () => {
+        const json = await (
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`))
+            .json();
+            setMovie(json.data.movie)
+    }
+    useEffect(() => {
+        getMovie();
+    }, [])
+    return (
+        <div>
+            <h1>{movie.title_long}</h1>
+            <img src={movie.medium_cover_image} />
+            <p>{movie.description_full}</p>
+        </div>
+    )
 }
-
-export default Detail
+export default Datail
--- movie-web-service/src/routes/Home.js
+++ movie-web-service/src/routes/Home.js
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 import Movie from '../components/Movie';
 
 function Home() {
-    const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+
   const getMovies = async() => {
     const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year")).json();
     setMovies(json.data.movies);
@@ -17,7 +18,7 @@ function Home() {
       {loading ? <h1>Loading...</h1> : 
         (
           <div>
-            {movies.map((movie)=><Movie coverImg={movie.medium_cover_image} title={movie.title} genres={movie.genres} />)}
+            {movies.map((movie)=><Movie key={movie.id} id={movie.id} coverImg={movie.medium_cover_image} title={movie.title} genres={movie.genres} />)}
           </div>
         )
       }
