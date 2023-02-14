75f7de9e5bba99e6943dd6cc8c26b27ccdd96428
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-14 10:49:59 +0900

    istall styled-compnents

--- master-class/src/App.js
+++ master-class/src/App.js
@@ -1,11 +1,31 @@
-import './App.css';
+import styled from "styled-components";
 
 function App() {
   return (
-   <div>
-    
-   </div>
+   <Father>
+    <BoxOne>
+      <Text>Hyein zzang</Text>
+    </BoxOne>
+    <BoxTwo></BoxTwo>
+   </Father>
   );
 }
 
 export default App;
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: green;
+  width:100px;
+  height:100px;
+`
+const BoxTwo = styled.div`
+  background-color: pink;
+  width:100px;
+  height:100px;
+`
+const Text = styled.span`
+  color: white;
+`
--- movie-web-service/src/routes/Detail.js
+++ movie-web-service/src/routes/Detail.js
@@ -1,39 +1,3 @@
-// import React, { useEffect, useState } from 'react'
-// import { useParams } from 'react-router-dom'
-
-// function Detail() {
-//     const [loading, setLoading] = useState(true);
-//     const [movies, setMovies] = useState([]);
-
-//     const {id} = useParams();
-//     console.log(id);
-    
-//     useEffect(()=>{
-//         getMovie();
-//     },[]);
- 
-//     const getMovie = async() => {
-//         const json = await(await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)).json();
-//         setMovies(json.data.movie);
-//         setLoading(false);
-//     }
-
-    
-//     console.log(movies);
-//   return (
-//     <div>
-//         <h1>hi</h1>
-//         {loading ? (<h1>Loading</h1>): (
-//             <div>
-//                 <div>{movies.title}</div>
-//             </div>
-//         )}
-//     </div>
-
-//   )
-// }
-// export default Detail;
-
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 function Datail() {
