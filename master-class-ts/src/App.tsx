ba7fdd2da1a3fc70383b4b26c5992d9cca5b6301
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-15 15:05:46 +0900

    forms

--- master-class-ts/src/App.tsx
+++ master-class-ts/src/App.tsx
@@ -1,24 +1,28 @@
+import { useState } from "react";
 import Circle from "./Circle";
 
 function App() {
+  const [value, setValue] = useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>) =>{
+    const {
+      currentTarget : {value},
+    } = event;
+    // const value = event.currnetTarget.value
+    setValue(value);
+  };
+  // event에 type 추가하기. ReactJs 내에 있는 React에서 FormEvent
+  // 어떤 종류의 Element가 이 onChange 이벤트를 발생시킬지 특정
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("hello", value);
+  }
   return (
    <div>
-    <Circle bgColor="teal" borderColor="black"/>
-    <Circle bgColor="green" text="green"/>
+    <form onSubmit={onSubmit}>
+      <input value={value} onChange={onChange} type="text" placeholder="username" />
+      <button>Log in</button>
+    </form>
    </div>
   );
 }
 export default App;
-
-// styled components ------------------------------
-// const Wrapper = styled.div`
-//   display: flex;
-//   height:100vh;
-//   width:100vw;
-//   justify-content: center;
-//   align-items: center;
-//   background-color: ${(props)=>props.theme.backgroundColor};
-// `;
-// const Title = styled.h1`
-//   color: ${(props) => props.theme.textColor};
-// `
