e18c627c059f3df8ec9c36705a1fe1e248b154da
Hyein <108849398+Joohyein@users.noreply.github.com>
2023-02-16 21:56:47 +0900

    add Router to the App.tsx

--- master-class-router/src/App.tsx
+++ master-class-router/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Router from './screens/Router';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Router />
     </div>
   );
 }
--- master-class-router/src/screens/Router.tsx
+++ master-class-router/src/screens/Router.tsx
@@ -4,13 +4,15 @@ import Home from "./Home";
 import About from "./About";
 
 function Router() {
-    return <BrowserRouter>
+    return (
+    <BrowserRouter>
         <Header />
         <Routes>
             <Route path="/" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
         </Routes>
     </BrowserRouter>
+    )
 }
 
 export default Router;
